0e9d52f1137c925d48de2d162b4eba85f2f3af23
MehediHasanRapiIIT <mehedi2515@student.nstu.edu.bd>
2025-09-04 03:31:51 +0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function login(){
+
+        // echo Hash::make(123456);
+        // die;
+        return view('auth.login');
+    }
+
+    public function auth_login(Request $request){
+
+         $auth = Auth::attempt(
+            [
+                'email' => $request->email, 'password' => $request->password], true);
+        if($auth){
+            return redirect('panel/dashboard');
+        }
+        else{
+            return redirect()->back()->with('error', 'Please enter correct email and password');
+        }
+    }
+
+    public function forgot(){
+        return view('auth.forgot');
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\SchoolController;
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/", [AuthController::class, 'login']);
+
+Route::post("/", [AuthController::class, 'auth_login']);
+
+Route::get("forgot", [AuthController::class, 'forgot']);
+
+Route::get("logout", [AuthController::class, 'logout']);
+
+Route::group(['middleware'=>'common'], function () {
+
+    Route::get("panel/dashboard", [DashboardController::class, 'dashboard']);
+
+    Route::get("panel/school", [SchoolController::class, 'school_list']);
+
+    Route::get("panel/school/create", [SchoolController::class, 'create_school']);
+
+    Route::post("panel/school/create", [SchoolController::class, 'insert_school']);
+});
